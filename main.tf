32dadcfd9dc60fcc5e337e7cf25b3f229d3c9a9c
a-noubissi <132452187+a-noubissi@users.noreply.github.com>
2024-11-14 16:30:19 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ locals {
 
 resource "google_compute_global_forwarding_rule" "fwd_rule" {
   provider              = google.project
-  name                  = "${local.cloudrun_trimmed_name}-forwarding-rule"
+  name                  = "${local.cloudrun_trimmed_name}--${terraform.workspace}-forwarding-rule"
   target                = google_compute_target_https_proxy.default.id
   load_balancing_scheme = "EXTERNAL_MANAGED"
   ip_address            = var.ip_address
